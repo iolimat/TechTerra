0aa773fd83c3f691611f5aa39c6ea220637c86bf
Mutasem Tamimi <88652105+Mutasem-Tamimi@users.noreply.github.com>
2024-04-27 06:54:59 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from Google_Search import *
+from Fetch_Page import *
+
+def main():
+    query = input("Enter your search query: ")
+    # num_results = int(input("Enter the number of search results to fetch: "))
+    while True:
+        try:
+            num_results = int(input("Enter the number of search results to fetch: "))
+            break  # Exit the loop if input is valid
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    while True:
+        try:
+            num_depth = int(input("Enter the number of depth for scraping: "))
+            break  # Exit the loop if input is valid
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    visitedSite.clear()
+    links = [(i,num_depth) for i in scrape_google_search(query,num_results)]
+    collection = []
+
+    for (link, depth) in links:
+        try:
+            paragraphs, pagelinks = fetchPage(link, depth)
+        except Exception as e:
+            continue
+        collection.append(paragraphs)
+        if pagelinks != None:
+            for i in pagelinks:
+                links.append((i,depth-1))
+    print(len(collection))
+
+    with open('Mark search.txt', 'w', encoding='utf-8') as f:
+        for item in collection:
+            f.write(str(item) + '\n')
+
+
+if __name__ == "__main__":
+    main()
